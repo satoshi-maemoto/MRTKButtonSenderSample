7d91b14f0c0b854dd43cf516abeff47fed892cf2
satoshi-maemoto <peugeot.106.s16@gmail.com>
2020-06-06 22:15:08 +0900

    Getting the event sender object

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public ToolTip toolTip;
+
+    public void ButtonClicked()
+    {
+        this.toolTip.ToolTipText = this.GetEventSender()?.name;
+    }
+
+    private GameObject GetEventSender()
+    {
+        foreach (var inputSource in CoreServices.InputSystem.DetectedInputSources)
+        {
+            foreach (var pointer in inputSource.Pointers)
+            {
+                if (pointer.IsInteractionEnabled)
+                {
+                    var sender = CoreServices.InputSystem.FocusProvider.GetFocusedObject(pointer);
+                    if (sender?.GetComponent<Interactable>() != null)
+                    {
+                        return sender;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
